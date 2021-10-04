d41f1786afe0915d9a00f8171121b081f94690ae
Student <jirka.pinkas@gmail.com>
2021-10-04 14:40:54 +0200

    constructor dependency injection + lombok

--- helloworld/src/main/java/cz/gopas/skoleni/service/ItemService.java
+++ helloworld/src/main/java/cz/gopas/skoleni/service/ItemService.java
@@ -1,14 +1,19 @@
 package cz.gopas.skoleni.service;
 
 import cz.gopas.skoleni.repository.DummyItemRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ItemService {
 
-    @Autowired
-    private DummyItemRepository itemRepository;
+    private final DummyItemRepository itemRepository;
+
+//    public ItemService(DummyItemRepository itemRepository) {
+//        this.itemRepository = itemRepository;
+//    }
 
     public String getItemCount() {
         return "count = " + itemRepository.count();
