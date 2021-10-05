fcf594202a4cb24e66b556d893373c647d704f3d
Student <jirka.pinkas@gmail.com>
2021-10-05 12:14:14 +0200

    spring boot project

---
+++ eshopweb/src/main/java/cz/gopas/eshopweb/EshopwebApplication.java
@@ -0,0 +1,13 @@
+package cz.gopas.eshopweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshopwebApplication.class, args);
+    }
+
+}
---
+++ eshopweb/src/test/java/cz/gopas/eshopweb/EshopwebApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.gopas.eshopweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
